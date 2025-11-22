ea2387929b5fc3fe8a133122cba966c1bab3646c
Avertenandor <youremail@example.com>
2025-11-22 08:55:15 +0200

    fix: Export get_user_language from bot.i18n module

--- bot/i18n/__init__.py
+++ bot/i18n/__init__.py
@@ -4,12 +4,13 @@ Internationalization (i18n) support.
 Provides translation functions and language management.
 """
 
-from .loader import get_translator, set_user_language
+from .loader import get_translator, set_user_language, get_user_language
 from .locales import SUPPORTED_LANGUAGES, DEFAULT_LANGUAGE
 
 __all__ = [
     "get_translator",
     "set_user_language",
+    "get_user_language",
     "SUPPORTED_LANGUAGES",
     "DEFAULT_LANGUAGE",
 ]
