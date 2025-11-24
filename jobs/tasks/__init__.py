e0a848c9c56177ee34d7a9fb28a862d61fa24627
Avertenandor <youremail@example.com>
2025-11-25 00:13:38 +0200

    fix: Add missing task imports

--- jobs/tasks/__init__.py
+++ jobs/tasks/__init__.py
@@ -14,6 +14,14 @@ from jobs.tasks.payment_retry import process_payment_retries
 from jobs.tasks.metrics_monitor import monitor_metrics
 from jobs.tasks.node_health_monitor import monitor_node_health
 from jobs.tasks.stuck_transaction_monitor import monitor_stuck_transactions
+from jobs.tasks.notification_fallback import process_notification_fallback
+from jobs.tasks.warmup_redis_cache import warmup_redis_cache
+from jobs.tasks.admin_session_cleanup import cleanup_expired_admin_sessions
+from jobs.tasks.mark_immutable_audit_logs import mark_immutable_audit_logs
+from jobs.tasks.redis_recovery import recover_redis_data
+from jobs.tasks.notification_fallback_processor import (
+    process_notification_fallback as process_notification_fallback_v2,
+)
 
 __all__ = [
     "process_daily_rewards",
@@ -24,4 +32,10 @@ __all__ = [
     "monitor_node_health",
     "monitor_metrics",
     "perform_financial_reconciliation",
+    "process_notification_fallback",
+    "warmup_redis_cache",
+    "cleanup_expired_admin_sessions",
+    "mark_immutable_audit_logs",
+    "recover_redis_data",
+    "process_notification_fallback_v2",
 ]
